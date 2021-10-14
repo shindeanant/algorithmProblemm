1c27d63ef80707b4d1581d774662ce9036a18063
shindeanant <shindeanant1997@gmail.com>
2021-10-14 16:17:30 +0530

    Added: Inseration sort added

---
+++ main/java/com/bridgelabzz/algorithmproblemdatastructure/InsertionSort.java
@@ -0,0 +1,16 @@
+package com.bridgelabzz.algorithmproblemdatastructure;
+
+public class InsertionSort {
+	public static <T extends Comparable<T>> void insertionSort(T[] arr) {
+		for (int i = 0; i < arr.length; i++) {
+			T temp = arr[i];
+			int j = i;
+			while (j > 0 && arr[j - 1].compareTo(temp) > 0) {
+				arr[j] = arr[j - 1];
+				j--;
+			}
+			arr[j] = temp;
+		}
+	}
+
+}
